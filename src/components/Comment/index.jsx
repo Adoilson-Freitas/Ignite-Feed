93d024c7c6feae566965e433c35cc3130f366a8d
Adoilson-Freitas <adoilsonbacelar@gmail.com>
2022-07-08 16:26:11 -0300

    Create comment component

---
+++ src/components/Comment/index.jsx
@@ -0,0 +1,38 @@
+import { ThumbsUp, Trash } from "phosphor-react";
+import styles from "./styles.module.css";
+
+export function Comment() {
+  return (
+    <div className={styles.comment}>
+      <img src="https://github.com/Adoilson-Freitas.png" />
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Adoilson Freitas</strong>
+              <time
+                title="06 de Julho às 15:37h"
+                dateTime="2022-07-06 15:37:30"
+              >
+                Cerca de 1h atrás
+              </time>
+            </div>
+            <button title="Deletar Comentário">
+              <Trash size={24} />
+            </button>
+          </header>
+
+          <p>Muito bom Devon, parabéns!! 👏👏</p>
+        </div>
+
+        <footer>
+          <button>
+            <ThumbsUp />
+            Aplaudir <span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  );
+}
